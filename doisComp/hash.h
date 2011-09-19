1794262ad1390353558babaddb2b083ea2c87ffe
aleonhardt <ale.lionheart@gmail.com>
2011-09-18 22:10:30 -0300

    hash now according to what's demmanded

--- doisComp/hash.h
+++ doisComp/hash.h
@@ -9,6 +9,17 @@ typedef struct _node{
 
 #define HASH_SIZE 997
 
+#define SYMBOL_UNDEFINED 0
+#define SYMBOL_LIT_INTEGER 1
+#define SYMBOL_LIT_TRUE 3
+#define SYMBOL_LIT_FALSE 4
+#define SYMBOL_LIT_CHAR 5
+#define SYMBOL_LIT_STRING 6
+#define SYMBOL_IDENTIFIER 7
+
+
+
+
 void initHash(node** hashTable);
 
 node* insertHash(node** hashTable, char* text, int type);
