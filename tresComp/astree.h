9b1d46b6912478dbb1b067338a62c90b0d112717
Arthur Rauter <rauter@ubuntu.(none)>
2011-10-04 22:02:24 -0300

    Parser revisado, lista de defines realmente usados

--- tresComp/astree.h
+++ tresComp/astree.h
@@ -62,6 +62,48 @@
 #define AST_control			51
 #define AST_vecatrib        56
 
+
+/* DEFINES usados no Parser:
+#define AST_declr	        47
+#define AST_vardecl 	    45
+#define AST_identifier      20
+#define AST_var             2
+#define AST_vector          14 
+#define AST_literal         27
+#define AST_kwchar          5
+#define AST_kwint           6
+#define AST_fundecl	        46
+#define AST_function        3
+#define AST_param           21 
+#define AST_listparam       22
+#define AST_block        23
+#define AST_cmdblock       24
+#define AST_cmd             25
+#define AST_control			51
+#define AST_kwprint         11
+#define AST_kwreturn        12
+#define AST_kwread          13
+#define AST_vecatrib        56		
+#define AST_atrib           19
+#define AST_expr	        34
+#define AST_logicexpr       40
+#define AST_aritexpr        26
+#define AST_sub             15
+#define AST_mul             16
+#define AST_div             17
+#define AST_add             18
+#define AST_resto	        37
+#define AST_operle 	        28
+#define AST_operge 	        29
+#define AST_opereq 	        30
+#define AST_operne 	        31
+#define AST_operor 	        32
+#define AST_operand         33
+#define AST_kwif            7
+#define AST_kwelse          9
+#define AST_kwwhile         10
+*/
+
 		
 
 //nodo e funções
@@ -79,11 +121,14 @@ AST *root;
 AST* ast_insert_node(int type, node *hashNode, AST *son0, AST *son1, AST *son2, AST *son3);
 //AST* ast_do_for_each(AST *node, void* (*fun)(AST*));
 void* ast_print(AST *node);
+/*
 char *print0(AST* node);
 char *print1(AST* node);
 char *print2(AST* node);
 char *print3(AST* node);
 char *print4(AST* node);
+*/
 void ast_to_program(AST *tree, FILE* outAST);
+
 
 #endif
